0247c70be71dfdfac41d313aa9d519720dd6f16e
Tristan <yanggc888@163.com>
2018-07-18 18:14:06 +0800

    update

---
+++ config/setting-baidu.js
@@ -0,0 +1,10 @@
+module.exports = {
+  resolve: `gatsby-plugin-baidu-analytics`,
+  options: {
+    // baidu analytics siteid
+    siteId: "1cae7396cd8c7648b6ff23a9123052ba",
+    // 配置统计脚本插入位置，默认值为 false, 表示插入到 body 中, 为 true 时插入脚本到 head 中
+    head: true,
+  },
+};
+

e8f9067e502fdc9da3ffb202e91e763d74c835ce
Tristan <yanggc888@163.com>
2018-07-20 14:09:02 +0800

    update: wrap out comment component

---
+++ src/components/Comment.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { DiscussionEmbed } from "disqus-react";
+
+
+export default class Comment extends Component {
+    render() {
+        const { config } = this.props;
+        return (
+            <DiscussionEmbed shortname={`blog-yangerxiao-com`} config={config} />
+        )
+    }
+}

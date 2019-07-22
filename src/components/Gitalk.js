6c39d334060a17f516e84bfe64793d471c69673f
tristan <yangguochun@ddweilai.com>
2019-07-22 18:24:43 +0800

    feat: update gitalk styles

--- src/components/Gitalk.js
+++ src/components/Gitalk.js
@@ -9,8 +9,37 @@ const DevWrapper = styled.div`
   padding: 1rem;
   border: 1px solid #ddd;
 `;
+const GTContainer = styled.div`
+  .gt-btn {
+    border-color: #333;
+    background-color: #000;
+    &:hover {
+      border-color: #666;
+      background-color: #000;
+    }
+  }
+  .gt-btn-preview {
+    color: #ccc;
+  }
+  .gt-header-controls-tip,
+  .gt-comment-username,
+  a,
+  .gt-link {
+    color: #222;
+    border-bottom-color: #ccc;
+    &:hover {
+      color: #666;
+      border-bottom-color: #333;
+    }
+  }
+  .gt-svg svg,
+  .gt-user .is--poping .gt-ico svg {
+    fill: #333;
+  }
+`;
 export default function Gitalk() {
-  const [isProd] = useState(process.env.NODE_ENV == 'production');
+  //   const [isProd] = useState(process.env.NODE_ENV == 'production');
+  const [isProd] = useState(process.env.NODE_ENV == 'development');
   useEffect(() => {
     if (isProd) {
       const language =
@@ -29,7 +58,7 @@ export default function Gitalk() {
     }
   }, [isProd]);
   return isProd ? (
-    <div id="gitalk-container" />
+    <GTContainer id="gitalk-container" />
   ) : (
     <DevWrapper>开发环境不加载评论组件</DevWrapper>
   );

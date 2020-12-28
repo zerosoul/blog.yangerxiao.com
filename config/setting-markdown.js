4122e90ad9dff6e17039259e9284c0b5b3b67ef2
Tristan <yanggc888@163.com>
2020-12-28 14:42:20 +0800

    fix: remove sharp dep

--- config/setting-markdown.js
+++ config/setting-markdown.js
@@ -1,30 +1,30 @@
 module.exports = {
-  resolve: 'gatsby-transformer-remark',
+  resolve: "gatsby-transformer-remark",
   options: {
     plugins: [
-      {
-        resolve: `gatsby-remark-images`,
-        options: {
-          maxWidth: 700,
-          showCaptions: true
-        }
-      },
+      // {
+      //   resolve: `gatsby-remark-images`,
+      //   options: {
+      //     maxWidth: 700,
+      //     showCaptions: true
+      //   }
+      // },
 
       {
-        resolve: 'gatsby-remark-prismjs',
+        resolve: "gatsby-remark-prismjs",
         options: {
           inlineCodeMarker: null,
-          noInlineHighlight: true
-        }
+          noInlineHighlight: true,
+        },
       },
 
       {
         resolve: `gatsby-remark-autolink-headers`,
         options: {
-          offsetY: `100`
-        }
+          offsetY: `100`,
+        },
       },
-      `gatsby-remark-responsive-iframe`
-    ]
-  }
+      `gatsby-remark-responsive-iframe`,
+    ],
+  },
 };
